4eec558846a13ae0a7343fad0e072a0015f54a96
Malachi Scott <100097327+MalachiScott2448@users.noreply.github.com>
2023-02-19 14:55:03 -0600

    Update java.js

--- Java/java.js
+++ Java/java.js
@@ -1,1 +1,25 @@
+/*  JavaScript 7th Edition
+    
+    Author: Malachi Scott
+    Date:   02/16/2023
 
+    Filename: java.js
+*/
+const currentTime = new Date();
+const currentHour = currentTime.getHours();
+
+const body = document.querySelector('body');
+
+if (currentHour < 12) {
+  body.style.backgroundColor = '#999997';
+} else if (currentHour < 18) {
+  body.style.backgroundColor = '';
+} else {
+  body.style.backgroundColor = '#595958';
+}
+
+let sidebarToggle = document.getElementById("side-toggle");
+sidebarToggle.addEventListener("click", function(){
+    let sidebar = document.querySelector(".aside");
+    sidebar.classList.toggle("collapsed");
+});
