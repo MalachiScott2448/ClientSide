984b31802e8a12814ecf26208f1ec07bc73e4bc8
Malachi Scott <100097327+MalachiScott2448@users.noreply.github.com>
2023-03-25 04:20:18 -0500

    Update java2.js

--- Java/java2.js
+++ Java/java2.js
@@ -25,6 +25,20 @@ noButton.addEventListener("click", function(){
   result.classList.toggle("collapsed");
 });
 
+let submitButton = document.getElementById("submitButton");
+let pwd = document.getElementById("pwd");
+let pwd2 = document.getElementById("pwd2");
+
+submitButton.addEventListener('click', function() {
+      if (!/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(pwd.value)) {
+            validationMessage.textContent = 'Your password must be at least 8 characters with at least one letter and one number.';
+          } else if (pwd.value !== pwd2.value) {
+            validationMessage.textContent = 'Your passwords must match.';
+          } else {
+            validationMessage.textContent = '';
+          }
+    });
+
 // Allow pricing for future payments
 function formatPrice(value) {
   return "$" + value.toFixed(2);
