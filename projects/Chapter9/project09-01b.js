3803b3488fb2cb262c29ce893375f49102bd8f95
Malachi Scott <100097327+MalachiScott2448@users.noreply.github.com>
2023-04-13 23:21:36 -0500

    Create project09-01b.js

---
+++ projects/Chapter9/project09-01b.js
@@ -0,0 +1,27 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 9
+      Project 09-01
+
+      Project to read field values from a query string
+      Author: Malachi Scott
+      Date:   04/13/2023
+
+      Filename: project09-01b.js
+*/
+
+let query = location.search.slice(1);
+   
+  query = query.replace(/\+/g, " ");
+  
+  query = decodeURIComponent(query);
+  
+  const cardFields = query.split(/&/g);
+      
+  for(let items of cardFields) {
+      let nameValue = items.split(/=/);
+      let name = nameValue[0];
+      let value = nameValue[1];
+    
+     document.getElementById(name).textContent = value;
+  }
