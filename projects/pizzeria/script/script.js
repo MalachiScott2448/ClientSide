3e3b4670bccc61a4741a7f83231e54f308682cc7
Malachi Scott <100097327+MalachiScott2448@users.noreply.github.com>
2023-08-21 17:50:45 -0500

    Update and rename index.html to script.js

---
+++ projects/pizzeria/script/script.js
@@ -0,0 +1,130 @@
+"use strict"
+
+
+
+let item1 = document.getElementById('item1');
+let pizzaImg3 = document.getElementById('pizzaImg3');
+let pizzaCrust = document.getElementById('pizzaCrust');
+
+item1.addEventListener('change', function() {
+    if (item1.checked) {
+      pizzaImg3.style.display = "block";
+      pizzaImg3.style.position = "absolute";
+      pizzaCrust.style.position = "absolute";
+    } else {
+      pizzaImg3.style.display = "none";
+      pizzaCrust.style.position = "absolute";
+    }
+  });
+
+let item2 = document.getElementById('item2');
+let pizzaImg4 = document.getElementById('pizzaImg4');
+
+item2.addEventListener('change', function() {
+    if (item2.checked) {
+      pizzaImg4.style.display = "block";
+      pizzaImg4.style.position = "absolute";
+      pizzaCrust.style.position = "absolute";
+    } else {
+      pizzaImg4.style.display = "none";
+      pizzaCrust.style.position = "absolute";
+    }
+  });
+
+let item4 = document.getElementById('item4');
+let pizzaImg5 = document.getElementById('pizzaImg5');
+
+item4.addEventListener('change', function() {
+    if (item4.checked) {
+      pizzaImg5.style.display = "block";
+      pizzaImg5.style.position = "absolute";
+      pizzaCrust.style.position = "absolute";
+    } else {
+      pizzaImg5.style.display = "none";
+      pizzaCrust.style.position = "absolute";
+    }
+  });
+
+let item6 = document.getElementById('item6');
+let pizzaImg6 = document.getElementById('pizzaImg6');
+  
+item6.addEventListener('change', function() {
+    if (item6.checked) {
+      pizzaImg6.style.display = "block";
+      pizzaImg6.style.position = "absolute";
+      pizzaCrust.style.position = "absolute";
+    } else {
+      pizzaImg6.style.display = "none";
+      pizzaCrust.style.position = "absolute";
+    }
+  });
+
+let item8 = document.getElementById('item8');
+let pizzaImg7 = document.getElementById('pizzaImg7');
+
+item8.addEventListener('change', function() {
+    if (item8.checked) {
+      pizzaImg7.style.display = "block";
+      pizzaImg7.style.position = "absolute";
+      pizzaCrust.style.position = "absolute";
+    } else {
+      pizzaImg7.style.display = "none";
+      pizzaCrust.style.position = "absolute";
+    }
+  });
+
+let item10 = document.getElementById('item10');
+let pizzaImg8 = document.getElementById('pizzaImg8');
+
+item10.addEventListener('change', function() {
+    if (item10.checked) {
+      pizzaImg8.style.display = "block";
+      pizzaImg8.style.position = "absolute";
+      pizzaCrust.style.position = "absolute";
+    } else {
+      pizzaImg8.style.display = "none";
+      pizzaCrust.style.position = "absolute";
+    }
+  });
+
+let item7 = document.getElementById('item7');
+let pizzaImg9 = document.getElementById('pizzaImg9');
+
+item7.addEventListener('change', function() {
+    if (item7.checked) {
+      pizzaImg9.style.display = "block";
+      pizzaImg9.style.position = "absolute";
+      pizzaCrust.style.position = "absolute";
+    } else {
+      pizzaImg9.style.display = "none";
+      pizzaCrust.style.position = "absolute";
+    }
+  });
+
+let item5 = document.getElementById('item5');
+let pizzaImg10 = document.getElementById('pizzaImg10');
+  
+item5.addEventListener('change', function() {
+    if (item5.checked) {
+      pizzaImg10.style.display = "block";
+      pizzaImg10.style.position = "absolute";
+      pizzaCrust.style.position = "absolute";
+    } else {
+      pizzaImg10.style.display = "none";
+      pizzaCrust.style.position = "absolute";
+    }
+  });
+
+let item3 = document.getElementById('item3');
+let pizzaImg11 = document.getElementById('pizzaImg11');
+    
+item3.addEventListener('change', function() {
+    if (item3.checked) {
+      pizzaImg11.style.display = "block";
+      pizzaImg11.style.position = "absolute";
+      pizzaCrust.style.position = "absolute";
+    } else {
+      pizzaImg11.style.display = "none";
+      pizzaCrust.style.position = "absolute";
+    }
+  });
